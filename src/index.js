16e347216bffb330857ad0a8c8e4ccd0b9bc043d
Fernando <f.luhis@gmail.com>
2022-08-13 16:36:33 -0300

    Alteracao de find para findeIndex

--- src/index.js
+++ src/index.js
@@ -100,15 +100,15 @@ app.delete("/todos/:id", checksExistsUserAccount, (request, response) => {
   const { user } = request;
   const { id } = request.params;
 
-  const todo = user.todos.find((todo) => todo.id === id);
+  const todoIndex = user.todos.findIndex((todo) => todo.id === id);
 
-  if (!todo) {
+  if (todoIndex === -1) {
     return response.status(404).json({ error: "Todo not found" });
   }
 
-  user.todos = user.todos.filter((todo) => todo.id !== id);
+  user.todos.splice(todoIndex, 1);
 
-  return response.status(204).send();
+  return response.status(204).json();
 });
 
 module.exports = app;
